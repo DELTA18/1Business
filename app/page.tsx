4f263b6d1400b075323a11f0d10f7845c79bf9ae
DELTA18 <rajmalave4@gmail.com>
2025-07-11 12:39:53 +0530

    oauth google connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
 
 import { Button } from "@/components/ui/button"
-export default function Home() {
+// import  signIn  from "@/components/signIn"/
+import { signIn,  } from "@/auth"
+import { auth } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
   return (
     <main className="flex text-amber-200 flex-col items-center justify-between p-24">
       <h1>Home</h1>
       <Button variant={"secondary"}>Lessgoo</Button>
+      {session ? (
+        <>
+        
+        <p className=" text-4xl text-green-600 p-3">signed in as {session.user?.email}</p>
+        <p className=" text-4xl text-green-600 p-3"> {session.user?.name} </p>
+        </>
+      ) : (
+        <>
+        
+        <p>not signed in</p>
+        <form
+            action={async () => {
+              "use server"
+              await signIn("google")
+            }}
+          >
+            <button type="submit">Signin with Google</button>
+          </form>
+        </>
+      )}
+
+
+      
     </main>
   );
 }
