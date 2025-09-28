7e5c3d302cde8380caad1a566df700d4f0e4f4f3
DELTA18 <rajmalave4@gmail.com>
2025-09-28 16:35:52 +0530

    final this is it pleaseeeee

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,15 +1,16 @@
-"use server";
+"use client";
 
-import { signIn } from "@/app/api/auth/[...nextauth]/route";
+import { signIn } from "next-auth/react";
 
-export default async function SignInForm() {
+export default function SignInForm() {
   return (
-    <form
-      action={async () => {
-        await signIn("google");
-      }}
-    >
-      <button type="submit">Signin with Google</button>
-    </form>
+    <div className="flex justify-center mt-20">
+      <button
+        onClick={() => signIn("google")}
+        className="px-6 py-3 bg-cyan-600 text-white rounded-lg hover:bg-cyan-700"
+      >
+        Sign in with Google
+      </button>
+    </div>
   );
 }
